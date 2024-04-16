2ee2acd5fe4d6df563338a14a7140429172b5499
Cristina Miquel Garrido <miquc750@newschool.edu>
2024-04-15 23:00:14 -0400

    test graph

--- main.js
+++ main.js
@@ -4,46 +4,6 @@ let markdownIt = document.createElement('script')
 markdownIt.src = 'https://cdn.jsdelivr.net/npm/markdown-it@14.0.0/dist/markdown-it.min.js'
 document.head.appendChild(markdownIt) */
 
-// Chart
-var ctx = document.getElementById('myRadarChart').getContext('2d');
-var myRadarChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-        labels: ['Cost of living', 'Safety', 'Weather', 'Transportation'],
-        datasets: [{
-            label: 'Valencia',
-            data: [${cityData.scoreCost}, ${cityData.scoreSafety}, ${cityData.scoreWeather}, ${cityData.scoreTransportation}],
-            backgroundColor: 'transparent',
-            borderColor: '#2376E4',
-            borderWidth: 1
-        },
-        {
-            label: 'Madrid',
-            data: [7, 6, 5, 9],
-            backgroundColor: 'transparent',
-            borderColor: '#F15211',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        elements: {
-            line: {
-                borderWidth: 3
-            }
-        },
-        scales: {
-            r: {
-                beginAtZero: true,
-                angleLines: {
-                    display: false
-                },
-                suggestedMin: 0,
-                suggestedMax: 10
-            }
-        }
-    }
-});
-
 /* Access Airtable API
 var Airtable = require('airtable');
 var base = new Airtable({apiKey: 'patyGtqiWtf3ypzpD.00631f93c77f8bb3f8c7b45f64a6a2b3d72575b51bac2e679507666590ac875a'}).base('appBL5bDGDsHFNRAh');
@@ -234,3 +194,55 @@ function setActiveButton(activeId) {
 
     document.getElementById(activeId).classList.add('button-active');
 }
+
+// Chart
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const data = await fetch('data.json').then(res => res.json());
+    const cityData = data.cities.find(c => c.city.toLowerCase() === cityName);  // Find the specific city data
+
+    if (cityData) {
+        setupButtons(cityData);
+
+        var ctx = document.getElementById('myRadarChart').getContext('2d');
+                var myRadarChart = new Chart(ctx, {
+                    type: 'radar',
+                    data: {
+                        labels: ['Cost of living', 'Safety', 'Weather', 'Transportation'],
+                        datasets: [{
+                            label: 'Valencia',
+                            data: [cityData.scoreCost, cityData.scoreSafety, cityData.scoreWeather, cityData.scoreTransportation],
+                            backgroundColor: 'transparent',
+                            borderColor: '#2376E4',
+                            borderWidth: 1
+                        },
+                        {
+                            label: 'Madrid',
+                            data: [7, 6, 5, 9],
+                            backgroundColor: 'transparent',
+                            borderColor: '#F15211',
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        elements: {
+                            line: {
+                                borderWidth: 3
+                            }
+                        },
+                        scales: {
+                            r: {
+                                beginAtZero: true,
+                                angleLines: {
+                                    display: false
+                                },
+                                suggestedMin: 0,
+                                suggestedMax: 10
+                            }
+                        }
+                    }
+                });
+            }
+
+            fetchAndRenderItems();
+        });
